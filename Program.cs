9ce86a2d1173c939d0d8ed17701410b64ac0e40a
Максим Груздев <maestrosuper@mail.ru>
2024-01-16 14:02:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using MonitoringTask;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+public class App
+{
+    /// <summary>
+    /// Создаю объект, инкапсулирующий методы системного мониторинга
+    /// </summary>
+    static readonly SystemInfo _systemInfo = new ();
+
+    // Переменные, куда буду сохранять полученные показания
+    private static string _cpuLoad = String.Empty;
+    private static string _memoryLoad = String.Empty;
+    public static void Main()
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+
+        // Создаю и стартую поток для мониторинга процессора
+        new Thread(() =>
+        {
+            while (true)
+            {
+                _cpuLoad = _systemInfo.GetCpu(); // Сохраняю показания
+                Thread.Sleep(1000); // Задаю периодичность в 1 сек
+            }
+        }).Start(); // Стартую поток
+
+        // Создаю и стратую поток для мониторинга памяти
+        new Thread(() =>
+        {
+            while (true)
+            {
+                _memoryLoad = _systemInfo.GetRam(); // Сохраняю показания
+                Thread.Sleep(500); // Задаю периодичность в 0,5 сек
+            }
+        }).Start(); // Стартую поток
+
+        // Вывожу показания в цикле основного потока
+        while (true)
+        {
+            Console.Clear(); // Очищаю консоль
+            Console.WriteLine($"Загрузка процессора: {_cpuLoad}{Environment.NewLine}Доступно памяти: {_memoryLoad}"); // Вывожу показания
+            Thread.Sleep(800); // Задаю периодичность обновления 0,8 сек
+        }
+    }
+}
---
+++ SystemInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace MonitoringTask
+{
+    /// <summary>
+    /// Позволяет получать информацию о свободной памяти RAM и загрузке процессора
+    /// </summary>
+    public class SystemInfo
+    {
+        private PerformanceCounter _cpuCounter = new("Processor", "% Processor Time", "_Total");
+        private PerformanceCounter _ramCouner = new("Memory", "Available MBytes");
+
+        /// <summary>
+        /// Задаю показания процессора в процентах
+        /// </summary>
+        /// <returns></returns>
+        public string GetCpu()
+        {
+            return _cpuCounter.NextValue() + " %";
+        }
+
+        /// <summary>
+        /// Задаю показания помяти в мегабайтах
+        /// </summary>
+        /// <returns></returns>
+        public string GetRam()
+        {
+            return _ramCouner.NextValue() + " MB";
+        }
+    }
+}
